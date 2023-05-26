cd0b2b6f622594c332d9cb16cb3ebcd6692243b7
Prathamesh Joshi <prathamesh231197@gmail.com>
2023-05-26 12:23:08 +0530

    loading icon

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import apiClient, { CanceledError } from "./services/api-client";
+
+interface User {
+  id: number;
+  name: String;
+}
+
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  const [error, setError] = useState("");
+
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+
+    apiClient
+      .get<User[]>("/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseTracker/ExpenseFilter.tsx
@@ -0,0 +1,25 @@
+import categories from "./categories";
+
+interface Props {
+  onSelectCategory: (category: String) => void;
+}
+
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
+  return (
+    <div>
+      <select
+        className="form-select"
+        onChange={(event) => onSelectCategory(event.target.value)}
+      >
+        <option value="">All categories</option>
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default ExpenseFilter;
